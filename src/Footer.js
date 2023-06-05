0572cac8c904b7e944516fde9c8bd467fb840b3d
Zgene <Zburdge4389@gmail.com>
2023-06-05 13:28:49 -0700

    Added Footer file and css

---
+++ src/Footer.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Footer.css";
+
+export default function Footer() {
+  return (
+    <small className="Footer">
+      <a
+        href="https://github.com/ZBurdge/My-weather-app.git"
+        alt="github-link"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Open-source code
+      </a>
+      , by Zgene' Burdge
+    </small>
+  );
+}
