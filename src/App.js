d732cb2f3a1390f163fe680f0e31912fe51cdfec
Zgene <Zburdge4389@gmail.com>
2023-06-05 13:14:16 -0700

    Added initial file structure

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Search from "./Search";
+import Location from "./Location";
+import City from "./City";
+import Weather from "./Weather";
+import Forecast from "./Forecast";
+import Footer from "./Footer";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="weather-app-wrapper">
+        <div className="weather-app">
+          <Search />
+          <Location />
+          <City />
+          <Weather />
+          <Forecast />
+        </div>
+        <Footer />
+      </div>
     </div>
   );
 }
-
 export default App;
