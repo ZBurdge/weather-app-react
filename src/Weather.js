e2ebebfa86bdfcb95662e36a1753a90cc5f8b63c
Zgene <Zburdge4389@gmail.com>
2023-06-05 16:00:53 -0700

    Added other files code to Weather file

--- src/Weather.js
+++ src/Weather.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./Weather.css";
 
 export default function Weather() {
+  let currentCity = { city: "Avondale" };
   let weatherData = {
     date: "Thursday 13:05",
     icon: "https://assets.msn.com/weathermapdata/1/static/weather/Icons/taskbar_v8/Condition_Card/MostlyCloudyDayV2.svg",
@@ -11,37 +12,59 @@ export default function Weather() {
     wind: "7",
   };
   return (
-    <div className="Weather row">
-      <div className="col-6">
-        <h2 className="currentWeather">
-          <section id="date-time">{weatherData.date}</section>
-          <div className="iconTemp">
-            <img
-              src={weatherData.icon}
-              alt={weatherData.description}
-              id="icon"
-            />
-            <strong id="current-temp">{weatherData.tempature}</strong>
-            <span className="units"> °F </span>
-          </div>
-        </h2>
+    <div className="weather">
+      <div className="Search">
+        {" "}
+        <form id="city-search">
+          <input
+            type="text"
+            placeholder=" search city..."
+            autoComplete="off"
+            id="searchBox"
+            className="searchBox"
+          />
+          <input type="submit" value="🔍" className="search-btn" />
+        </form>{" "}
       </div>
-      <div className="col-6">
-        <h3 className="weatherInfo">
-          <ul>
-            <li>
-              <span className="description" id="description">
-                {weatherData.description}
-              </span>
-            </li>
-            <li>
-              Humidity: <span id="humidity">{weatherData.humidity}</span>%
-            </li>
-            <li>
-              Wind: <span id="wind">{weatherData.wind}</span>mph
-            </li>
-          </ul>
-        </h3>
+
+      <button id="currentLocation-btn" className="Location">
+        Current Location
+      </button>
+      <h1 id="city" className="City">
+        {currentCity.city}
+      </h1>
+      <div className="row">
+        <div className="col-6">
+          <h2 className="currentWeather">
+            <section id="date-time">{weatherData.date}</section>
+            <div className="iconTemp">
+              <img
+                src={weatherData.icon}
+                alt={weatherData.description}
+                id="icon"
+              />
+              <strong id="current-temp">{weatherData.tempature}</strong>
+              <span className="units"> °F </span>
+            </div>
+          </h2>
+        </div>
+        <div className="col-6">
+          <h3 className="weatherInfo">
+            <ul>
+              <li>
+                <span className="description" id="description">
+                  {weatherData.description}
+                </span>
+              </li>
+              <li>
+                Humidity: <span id="humidity">{weatherData.humidity}</span>%
+              </li>
+              <li>
+                Wind: <span id="wind">{weatherData.wind}</span>mph
+              </li>
+            </ul>
+          </h3>
+        </div>
       </div>
     </div>
   );
