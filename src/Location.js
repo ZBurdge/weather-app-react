f105cbc998f2fd4a69a65618216da5d649db940d
Zgene <Zburdge4389@gmail.com>
2023-06-05 13:23:17 -0700

    Added location file & css

---
+++ src/Location.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./Location.css";
+
+export default function Location() {
+  return (
+    <header>
+      <button id="currentLocation-btn" className="Location">
+        Current Location
+      </button>
+    </header>
+  );
+}
